eada45a9e69bfd7c4a993615280258db305f8c79
benodiwal <s474996633@gmail.com>
2025-10-19 23:18:14 +0530

    fix: sphere rendering bug

--- src/main.zig
+++ src/main.zig
@@ -54,22 +54,28 @@ pub fn main() !void {
     const image_width = 400;
 
     // Ensuring image height is aleast 1
-    var image_height = @as(u64, @as(f64, @floatFromInt(image_width)) / aspect_ratio);
-    image_height = if (image_height < 1) 1 else image_height;
+    // Comptime block to save this calculation on compile time
+    const image_height = blk: {
+        const ih: comptime_int = @intFromFloat((image_width - 0.0)/aspect_ratio);
+        if (ih < 1) break :blk 1;
+        break :blk ih;
+    };
 
     // CAMERA
     const focal_length = 1.0;
     const viewport_height = 2.0;
-    const viewport_width = viewport_height * (@as(f64, @floatFromInt(image_width)) / @as(f64, @floatFromInt(image_height)));
+    const viewport_width = viewport_height * (image_width / image_height);
     const camera_center = Point3.init(0.0, 0.0, 0.0);
 
     const viewport_u = Vec3.init(viewport_width, 0.0, 0.0);
     const viewport_v = Vec3.init(0.0, -viewport_height, 0.0);
 
-    const pixel_delta_u = viewport_u.div(@as(f64, @floatFromInt(image_width)));
-    const pixel_delta_v = viewport_v.div(@as(f64, @floatFromInt(image_height)));
+    const pixel_delta_u = viewport_u.div(image_width);
+    const pixel_delta_v = viewport_v.div(image_height);
 
-    const viewport_upper_left = camera_center.sub(Vec3.init(0, 0, focal_length).sub(viewport_u.div(2)).sub(viewport_v.div(2)));
+    // Camera Center - (viewport_u/2) - (viewport_v/2) - (focal_length in z)
+    // Start from Camera center, move left, move up, move forward
+    const viewport_upper_left = camera_center.sub(Vec3.init(0, 0, focal_length)).sub(viewport_u.div(2)).sub(viewport_v.div(2));
     const pixel00_loc = viewport_upper_left.add(pixel_delta_u.add(pixel_delta_v).scale(0.5));
 
     try log_writer.writeAll("Starting ray tracer...\n");
@@ -84,7 +90,7 @@ pub fn main() !void {
         try log_writer.print("Processing row {d}/{d}\n", .{j+1, image_height});
 
         for (0..image_width) |i| {
-            const pixel_center = pixel00_loc.add(pixel_delta_u.scale(@as(f64, @floatFromInt(i)))).add(pixel_delta_v.scale(@as(f64, @floatFromInt(j))));
+            const pixel_center = pixel00_loc.add(pixel_delta_u.scale(@floatFromInt(i))).add(pixel_delta_v.scale(@floatFromInt(j)));
             const ray_direction = pixel_center.sub(camera_center);
             const r = Ray.init(&camera_center, &ray_direction);
             const pixel_color = try colorRay(&r, log_writer);
